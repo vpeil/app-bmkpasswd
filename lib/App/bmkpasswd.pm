1a25c427af4191ebbd315434a5361fdf97134a52
Vitali Peil <vitali.peil@uni-bielefeld.de>
2020-10-08 17:30:52 +0200

    add license to pm file

--- lib/App/bmkpasswd.pm
+++ lib/App/bmkpasswd.pm
@@ -433,6 +433,16 @@ Jon Portnoy <jon@portnoy.me>
 
 Vitali Peil <vitali.peil@uni-bielefeld.de>
 
+=head1 LICENSE AND COPYRIGHT
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 =for Pod::Coverage have_\w+ get_\w+
 
 =cut
